22d52804e3153ac3e42d2ee5ec8a67aba77c3aba
julien.valera@reseau.eseo.fr <pUwMpg63rFv6>
2018-10-11 17:05:27 +0200

    add datas_collect.py script

---
+++ BackupDatas/datas_collect.py
@@ -0,0 +1,54 @@
+import wget
+import os, errno
+from datetime import *
+import logging
+import shutil, filecmp, re
+import logging
+
+url_magickingdom = "https://parksapi.herokuapp.com/api/dlp-mk"
+url_studios = "https://parksapi.herokuapp.com/api/dlp-wds"
+user_path_directory = os.path.expanduser('~')
+today = datetime.today().strftime('%Y%m%d_%H%M%S')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+try:
+    os.makedirs(user_path_directory + "/DataRaw/Records")
+    os.makedirs(user_path_directory + "/DataRaw/LastRecovered")
+except OSError as e:
+    if e.errno != errno.EEXIST:
+        raise
+
+
+def fill_repo():
+    lastrecovered_path = user_path_directory + "/DataRaw/LastRecovered/"
+    records_path = user_path_directory + "/DataRaw/Records/"
+    filename_magickingdom = today + "_DisneylandParisMagicKingdom.json"
+    filename_studios= today + "_DisneylandParisWaltDisneyStudios.json"
+
+    try:
+        #Recovering and saving API JSON files to a directory
+        wget.download(url_magickingdom, records_path + filename_magickingdom)
+        wget.download(url_studios, records_path + filename_studios)
+
+        #Check if LastRecovered directory is empty
+        if len([name for name in os.listdir(lastrecovered_path)]) == 0:
+            shutil.copyfile(records_path + filename_magickingdom, lastrecovered_path + filename_magickingdom) #copy to Records directory
+            shutil.copyfile(records_path + filename_studios, lastrecovered_path + filename_studios) #copy to Records directory
+        elif len([name for name in os.listdir(lastrecovered_path)]) == 2:
+            for file in os.listdir(lastrecovered_path):
+                if "DisneylandParisMagicKingdom" == re.search("[0-9]{8}_[0-9]{6}_(\w*).json", file).group(1):
+                    if not filecmp.cmp(lastrecovered_path + file, records_path + filename_magickingdom):
+                        os.remove(lastrecovered_path + file)
+                        shutil.copyfile(records_path + filename_magickingdom, lastrecovered_path + filename_magickingdom)
+                elif "DisneylandParisWaltDisneyStudios" == re.search("[0-9]{8}_[0-9]{6}_(\w*).json", file).group(1):
+                    if not filecmp.cmp(lastrecovered_path + file, records_path + filename_studios):
+                        os.remove(lastrecovered_path + file)
+                        shutil.copyfile(records_path + filename_studios, lastrecovered_path + filename_studios)
+
+    except Exception as e:
+        logging.error('Error occured during the execution of fill_last_recovered_repo() method', exc_info=True)
+
+
+fill_repo()
